9db79c0157d441edc4f863114b3627ba56e4b44e
Léo-Paul COUTURIER <leopaul3@gmail.com>
2015-06-06 11:31:15 +0200

    Fix Base64

--- src/main/java/com/supinfo/todo/Utils.java
+++ src/main/java/com/supinfo/todo/Utils.java
@@ -1,9 +1,8 @@
 package com.supinfo.todo;
 
-import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
-
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 /**
  * Created by l3ops on 19/05/2015.
@@ -12,7 +11,7 @@ public class Utils {
     public static String sha1(String value) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-1"); // Crypte les mots de passe en SHA-1 pour la sécurité
-            return Base64.encode(md.digest(value.getBytes())); // Mot de passe stocké en base 64 plûtot q'en base 2 pour prendre moins de place
+            return Base64.getEncoder().encodeToString(md.digest(value.getBytes())); // Mot de passe stocké en base 64 plûtot q'en base 2 pour prendre moins de place
         } catch(NoSuchAlgorithmException e) { // Si le SHA-1 n'est pas disponible
             e.printStackTrace(); // Méthode pour l'afficher
             return null;
